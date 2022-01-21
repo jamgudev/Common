a7153c66b5db6917da0da729dfac73e9a5ac93a6
jamgudev <826630153@qq.com>
2022-01-21 20:46:34 +0800

    feat: 封装 BaseActivity。

---
+++ common/src/main/java/com/jamgu/common/page/activity/BaseActivity.kt
@@ -0,0 +1,77 @@
+package com.jamgu.common.page.activity
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.jamgu.base.util.JLog
+import com.jamgu.common.widget.dialog.CommonProgressDialog
+import com.jamgu.common.widget.toast.JToast
+
+
+/**
+ * Created by jamgu on 2022/01/21
+ *
+ * 最基础的 Activity，封装了 loading progress, toast, 两次返回点击监听
+ */
+open class BaseActivity: AppCompatActivity() {
+
+    private var mLastBackPressTime = -1L
+    private var mDialog: CommonProgressDialog? = null
+
+    companion object {
+        private const val TAG = "BaseActivity"
+        private const val EXIT_TIME_STAMP = 2000L
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val progressBar = ProgressBar(this)
+        progressBar.id = View.generateViewId()
+        progressBar.setBackgroundColor(Color.GRAY)
+        val progressBarLp: RelativeLayout.LayoutParams = RelativeLayout.LayoutParams(200, 200)
+        progressBar.layoutParams = progressBarLp
+    }
+
+    @JvmOverloads
+    protected fun showProgress(msg: String?, loadingDrawableId: Int? = null) {
+        if (mDialog == null) {
+            mDialog = if (loadingDrawableId == null) {
+                CommonProgressDialog.show(this, msg)
+            } else {
+                CommonProgressDialog.show(this, msg, resources.getDrawable(loadingDrawableId, null))
+            }
+        } else if (mDialog?.isShowing == false) {
+            mDialog?.apply {
+                setLoadingMsg(msg)
+                loadingDrawableId?.let {
+                    setLoadingDrawable(resources.getDrawable(it, null))
+                }
+                show()
+            }
+        }
+    }
+
+    protected fun hideProgress() {
+        mDialog?.hide()
+    }
+
+    @JvmOverloads
+    fun showToast(msg: String?, showLength: Int = Toast.LENGTH_SHORT) {
+        JToast.showToast(this, msg, showLength)
+    }
+
+    override fun onBackPressed() {
+        JLog.d(TAG, "onBackPressed() called.")
+        val now = System.currentTimeMillis()
+        if (mLastBackPressTime > 0 && now - mLastBackPressTime < EXIT_TIME_STAMP) {
+            super.onBackPressed()
+        } else {
+            showToast("再次点击退出")
+            mLastBackPressTime = System.currentTimeMillis()
+        }
+    }
+}
