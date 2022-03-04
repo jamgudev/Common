d5746497391bbc3f8d9ab500ed6ee488bec3c8eb
jamgudev <826630153@qq.com>
2022-03-04 21:35:18 +0800

    chore: update main project's code.

--- app/src/main/java/com/jamgu/common/MainActivity.kt
+++ app/src/main/java/com/jamgu/common/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.jamgu.common
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import com.jamgu.common.databinding.ActivityMainBinding
+import com.jamgu.common.page.activity.ViewBindingActivity
+import com.jamgu.common.util.log.JLog
+import com.jamgu.common.util.statusbar.StatusBarUtil
+import com.jamgu.common.util.timer.VATimer
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ViewBindingActivity<ActivityMainBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        StatusBarUtil.setStatusBarTransparent(this)
+        StatusBarUtil.fitStatusLayout(this, mBinding.toolbar, true)
+    }
+
+    val timer = VATimer()
+    override fun onResume() {
+        super.onResume()
+
+
+        timer.addOnEndListener {
+            JLog.d("MainActivity", "onEnd.")
+        }
+        timer.addOnRepeatListener {
+            JLog.d("MainActivity", "onRepeat.")
+        }
+        timer.run({
+            JLog.d("MainActivity", "timer clock. execution = $it repeat = ${timer.getCurrentRepeatCount()}")
+        }, 1000)
+//        ThreadPool.runUITask({
+//            timer.run({
+//                JLog.d("MainActivity", "timer clock. execution = $it repeat = ${timer.getCurrentRepeatCount()}")
+//            }, 1000)
+//        }, 5000)
+    }
+
+    override fun getViewBinding(): ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+    override fun onDestroy() {
+        super.onDestroy()
+        timer.release()
     }
 }
